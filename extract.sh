b5437d7aec4fea8891f2a7602acb90802d2a0f25
Deekshith SH <shdeekshith@gmail.com>
2023-10-01 09:08:57 +0000

    testing

--- extract.sh
+++ extract.sh
@@ -1,8 +1,8 @@
 read -p "Enter Build Name: " filename
-unzip $filename
+unzip $filename -d vose
 
 echo "Decompressing system.new.dat.br"
-brotli --decompress system.new.dat.br -o system.new.dat
+brotli --decompress vose/system.new.dat.br -o vose/system.new.dat
 if [ $? != 0 ]
 then
 echo "Stoped at decompressing system.new.dat.br"
@@ -10,7 +10,7 @@ exit 1
 fi
 
 echo "Extracting system.new.dat"
-python3 sdat2img.py system.transfer.list system.new.dat system.img
+python3 sdat2img.py vose/system.transfer.list vose/system.new.dat vose/system.img
 if [ $? != 0 ]
 then
 echo "Stoped at Extracting system.new.dat"
@@ -18,15 +18,15 @@ exit 1
 fi
 
 echo "Removing system.new.dat"
-rm system.new.dat
+rm vose/system.new.dat
 if [ $? != 0 ]
 then
-echo "Removing system.new.dat"
+echo "Stoping at Removing system.new.dat"
 exit 1
 fi
 
 echo "Decompressing vendor.new.dat.br"
-brotli --decompress vendor.new.dat.br -o vendor.new.dat
+brotli --decompress vose/vendor.new.dat.br -o vose/vendor.new.dat
 if [ $? != 0 ]
 then
 echo "Stoped at decompressing vendor.new.dat.br"
@@ -34,7 +34,7 @@ exit 1
 fi
 
 echo "Extracting vendor.new.dat"
-python3 sdat2img.py vendor.transfer.list vendor.new.dat vendor.img
+python3 sdat2img.py vose/vendor.transfer.list vose/vendor.new.dat vose/vendor.img
 if [ $? != 0 ]
 then
 echo "Stoped at Extracting vendor.new.dat"
@@ -42,20 +42,32 @@ exit 1
 fi
 
 echo "Removing vendor.new.dat"
-rm vendor.new.dat
+rm vose/vendor.new.dat
 if [ $? != 0 ]
 then
-echo "Removing vendor.new.dat"
+echo "Stoping at Removing vendor.new.dat"
 exit 1
 fi
 
+echo "Creating Folder system, vendor"
 mkdir "system"
 mkdir "vendor"
 
-sudo mount -r system.img system
-sudo mount -r vendor.img vendor
+echo "Mounting img"
+sudo mount -rw vose/system.img system
+sudo mount -rw vose/vendor.img vendor
 
-read -p "Press Enter to Unmount"
 
+sudo chown -R $UID system vendor
+
+echo "git add"
+git add .
+
+echo "git commit"
+git commit -m $filename
+
+echo "unmount"
 sudo umount "system"
 sudo umount "vendor"
+
+rm -r system vendor vose
