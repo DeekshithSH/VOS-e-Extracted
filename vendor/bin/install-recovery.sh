651d1994e20fc03fbf519920256c6f8073d9942a
Deekshith SH <shdeekshith@gmail.com>
2023-10-01 15:01:29 +0000

    voltage-2.8-Electrified-EOL-r5x-20230922-1424-UNOFFICIAL.zip

--- vendor/bin/install-recovery.sh
+++ vendor/bin/install-recovery.sh
@@ -1,9 +1,9 @@
 #!/vendor/bin/sh
-if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):67108864:c86b31c06e9db27128ee4dc4326bb047cbd2ce56; then
+if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):67108864:d08416282b8e6fffa56b2e9251bb69280b88d57c; then
   applypatch  \
           --patch /vendor/recovery-from-boot.p \
-          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):67108864:f6bc37039766f73d19f7dbf19b0b45f8ab51eabc \
-          --target EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):67108864:c86b31c06e9db27128ee4dc4326bb047cbd2ce56 && \
+          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):67108864:b1d0a71361ff54c33aa4d81e32b7bf1781119ae4 \
+          --target EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):67108864:d08416282b8e6fffa56b2e9251bb69280b88d57c && \
       log -t recovery "Installing new recovery image: succeeded" || \
       log -t recovery "Installing new recovery image: failed"
 else
